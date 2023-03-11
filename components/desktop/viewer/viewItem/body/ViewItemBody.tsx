be2b79f2822c3bd4f97da676fc1b93df32323bea
kigpand <kigpand@naver.com>
2023-03-11 20:16:11 +0900

    viewer tag 클릭시 검색처리. hashtag page에서 새로고침시깨지는 이슈 처리

--- components/desktop/viewer/viewItem/body/ViewItemBody.tsx
+++ components/desktop/viewer/viewItem/body/ViewItemBody.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { onSearchHashTag } from "../../../../../service/post";
 import useContent from "../../../../../store/content";
+import useData from "../../../../../store/data";
+import usePosts from "../../../../../store/post";
 import styles from "./ViewItemBody.module.scss";
 
 const ViewItemBody = () => {
-  const { currentContent } = useContent();
+  const { currentContent, clearCurrentContent } = useContent();
+  const { setHashTagPosts } = usePosts();
+  const { setSearchTag } = useData();
   const [tags, setTags] = useState<string[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     if (currentContent && currentContent.tag) {
@@ -16,13 +23,26 @@ const ViewItemBody = () => {
     }
   }, [currentContent]);
 
+  const onTagClick = async (tag: string) => {
+    const item = tag.substring(1);
+    const result = await onSearchHashTag(item);
+    if (result.data && result.data.length > 0) {
+      setHashTagPosts(result.data);
+      setSearchTag(item);
+      clearCurrentContent();
+      router.push("/hashTag");
+    } else {
+      alert("검색 결과가 없습니다.");
+    }
+  };
+
   return (
     <div className={styles.viewItemBody}>
       <div className={styles.content}>{currentContent.content}</div>
       <div className={styles.tags}>
         {tags.map((tag: string, i: number) => {
           return (
-            <div className={styles.tag} key={i}>
+            <div className={styles.tag} key={i} onClick={() => onTagClick(tag)}>
               {tag}
             </div>
           );
--- pages/hashTag.tsx
+++ pages/hashTag.tsx
@@ -1,10 +1,19 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import HashTagContents from "../components/desktop/hashTagContents/HashTagContents";
 import Viewer from "../components/desktop/viewer/Viewer";
 import useContent from "../store/content";
+import useData from "../store/data";
 import styles from "../styles/Home.module.scss";
 
 const HashTag = () => {
   const { currentContent } = useContent();
+  const { searchTag } = useData();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (searchTag === "") router.push("/post");
+  }, [searchTag]);
 
   return (
     <div className={styles.container}>
