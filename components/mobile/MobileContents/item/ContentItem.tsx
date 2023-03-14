0376601f7c3c2eef44ca0ccf8db26e11da13ba8f
kigpand <kigpand@naver.com>
2023-03-14 11:33:13 +0900

    모바일 뷰 답글 추가 기능 구현

--- components/mobile/MobileContents/item/ContentItem.tsx
+++ components/mobile/MobileContents/item/ContentItem.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import usePosts from "../../../../store/post";
+import AddComment from "../../../shared/addComment/AddComment";
 import ViewItemBody from "../../../shared/viewItem/body/ViewItemBody";
 import ViewItemComment from "../../../shared/viewItem/comment/ViewItemComment";
 import ViewItemHeader from "../../../shared/viewItem/header/ViewItemHeader";
@@ -11,6 +12,20 @@ interface IContentItem {
 
 const ContentItem = ({ post }: IContentItem) => {
   const { posts, setPosts } = usePosts();
+  const [updateItem, setUpdateItem] = useState<any>(null);
+
+  useEffect(() => {
+    if (updateItem) {
+      const result = posts.find((post: any) => post.id === updateItem.id);
+      if (result) {
+        setPosts([
+          updateItem,
+          ...posts.filter((post: any) => post.id !== updateItem.id),
+        ]);
+        setUpdateItem(null);
+      }
+    }
+  }, [updateItem]);
 
   return (
     <div className={styles.contentItem}>
@@ -27,6 +42,7 @@ const ContentItem = ({ post }: IContentItem) => {
       <div className={styles.texts}>
         <ViewItemBody viewItem={post} />
         <ViewItemComment viewItem={post} />
+        <AddComment viewItem={post} setViewItem={setUpdateItem} />
       </div>
     </div>
   );
