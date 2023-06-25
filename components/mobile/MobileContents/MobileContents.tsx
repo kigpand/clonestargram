eb4ccfe26243d764e579a0e886d5db12981f05bc
김지훈 <kigpand@gmail.com>
2023-06-25 21:38:10 +0900

    mobile scroll 구현

--- components/mobile/MobileContents/MobileContents.tsx
+++ components/mobile/MobileContents/MobileContents.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePost } from "../../../hooks/usePost";
 import { IPost } from "../../../interface/IPost";
 import usePosts from "../../../store/post";
@@ -11,6 +11,25 @@ import HashTagContents from "../../hashTagContents/HashTagContents";
 const MobileContents = () => {
   const { posts, isLoading, error } = usePost();
   const { post, hashTagPosts, setHashTagPosts, setPost } = usePosts();
+  const [arr, setArr] = useState<IPost[]>([]);
+  const [scroll, setScroll] = useState<number>(0);
+
+  const onScroll = () => {
+    if (
+      window.scrollY + document.documentElement.clientHeight >
+      document.documentElement.scrollHeight - 50
+    ) {
+      setScroll(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
 
   useEffect(() => {
     if (posts) {
@@ -28,13 +47,32 @@ const MobileContents = () => {
     };
   }, [posts]);
 
+  useEffect(() => {
+    if (post.length > 3) {
+      setArr([post[0], post[1], post[2]]);
+    } else {
+      setArr([...post]);
+    }
+  }, [post]);
+
+  useEffect(() => {
+    if (post.length - 1 === arr.length || post.length === 0 || scroll === 0) {
+      return;
+    }
+    setArr([...arr, post[arr.length + 1]]);
+  }, [scroll]);
+
+  useEffect(() => {
+    console.log(arr);
+  }, [arr]);
+
   return (
     <div className={styles.mobileContents}>
       {hashTagPosts.length > 0 ? (
         <HashTagContents isMobile={true} />
       ) : (
         <div className={styles.lists}>
-          {post?.map((item: IPost, i: number) => {
+          {arr.map((item: IPost, i: number) => {
             return <ContentItem post={item} key={i} />;
           })}
         </div>
