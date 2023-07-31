88301596765c04614d1df30d7c03e7a82ea3e400
김지훈 <kigpand@gmail.com>
2023-07-31 10:46:41 +0900

    cookie set 중간 저장

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,20 +1,32 @@
-import JWT from "jsonwebtoken";
+// import JWT from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
+import { onCheckUser } from "../../../service/user";
+import JWT from "jsonwebtoken";
 
-export async function POST(req: NextRequest) {
-  const accessTokenPayload = await req.json();
-  const accessToken = JWT.sign(
-    accessTokenPayload,
-    process.env.NEXT_PUBLIC_JWT_SECRET!
-  );
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { id, pw } = await req.json();
+  const accessToken = JWT.sign({ id }, process.env.NEXT_PUBLIC_JWT_SECRET!);
+  // const result = await onCheckUser(id, pw);
 
-  return new Response("success", {
-    status: 200,
-    headers: { "Set-Cookie": `token=${accessToken}` },
+  const response = NextResponse.json({ status: 200 });
+
+  // Then set a cookie
+  response.cookies.set({
+    name: "jwt",
+    value: accessToken,
+    httpOnly: true,
   });
+
+  return response;
 }
 
 export async function GET(req: NextRequest) {
-  console.log(req.cookies);
-  console.log("45646464");
+  const cookie = req.cookies;
+  console.log(cookie);
+  const value = cookie.get("jwt")?.value;
+  if (!value) return new Response("fail");
+  const jwt = JWT.verify(value, process.env.NEXT_PUBLIC_JWT_SECRET!);
+  console.log(jwt);
+
+  return NextResponse.json({ status: 200 });
 }
