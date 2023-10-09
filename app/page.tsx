3dcd416816b9b26b36e5dc1df24ce8c07e145b88
김지훈 <kigpand@gmail.com>
2023-10-09 16:22:04 +0900

    nextjs version update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.scss";
 import LoginComponents from "../components/shared/loginComponents/LoginComponents";
+import { unstable_getImgProps as getImgProps } from "next/image";
 
 const Home: NextPage = () => {
+  const common = { alt: "Hero", width: 800, height: 400 };
+  const {
+    props: { srcSet: light, ...rest },
+  } = getImgProps({ ...common, src: "/loginBack.jpg" });
+
   return (
     <div className={styles.login}>
-      <img src="/loginBack.jpg" className={styles.loginImg} alt="login"></img>
+      <img {...rest} className={styles.loginImg} alt="login"></img>
       <LoginComponents />
     </div>
   );
